e8d9d6dcc2a21e0a65658d9104f7d00939c5dccd
mek <mihir.h.vala@gmail.com>
2018-12-10 15:47:07 +0530

    - Update (Day 1)

--- pro8.py
+++ pro8.py
@@ -5,16 +5,16 @@ customer_id = 101
 bill_amount = 1200.00
 discount = 0
 
-print("bill_id: %d"%bill_id)
-print("customer_id: %d"%customer_id)
-print("bill_amount: Rs.%f"%bill_amount)
+print("bill_id: %d" % bill_id)
+print("customer_id: %d" % customer_id)
+print("bill_amount: Rs.%f" % bill_amount)
 
-if bill_amount >= 1000 :
+if bill_amount >= 1000:
     discount = 5
 elif 500 <= bill_amount < 1000:
     discount = 2
 elif 0 < bill_amount < 500:
     discount = 1
 
-discounted_amount = bill_amount - bill_amount * discount/ 100
-print("Discounted Bill Amount: Rs.%f" %discounted_amount)
+discounted_amount = bill_amount - bill_amount * discount / 100
+print("Discounted Bill Amount: Rs.%f" % discounted_amount)
